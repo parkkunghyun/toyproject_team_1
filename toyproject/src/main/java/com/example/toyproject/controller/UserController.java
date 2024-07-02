2d274bb76d9488aef82ccc2b22bb0ff0d75862cf
Ramel <dev_20@naver.com>
2024-07-02 22:56:09 +0900

    GitHub Login 구현

--- toyproject/src/main/java/com/example/toyproject/controller/UserController.java
+++ toyproject/src/main/java/com/example/toyproject/controller/UserController.java
@@ -1,22 +1,17 @@
 package com.example.toyproject.controller;
 
-import com.example.toyproject.service.UserService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequiredArgsConstructor
-@Controller
-@RequestMapping("/user")
+@RestController
 public class UserController {
-    private final UserService userService;
-
-    @GetMapping("/login")
-    public String login() {
-        return "signIn_form";
+    @GetMapping("/")
+    public String home() {
+        return "Hello, home";
     }
 
-
-
+    @GetMapping("/secured")
+    public String secured() {
+        return "Hello, secured";
+    }
 }
--- toyproject/src/main/java/com/example/toyproject/service/DataNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.toyproject.service;
-
-public class DataNotFoundException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-    public DataNotFoundException(String s) {
-        super(s);
-    }
-}
